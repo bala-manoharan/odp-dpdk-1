6e2b2da8a984cc1c396b2fb06cf04f040cf083b9
Balasubramanian Manoharan <Balasubramanian.Manoharan@caviumnetworks.com>
2018-07-02 17:44:23 +0000

    test: odp_fwd: remove unneeded assert
    Signed-off-by: Balasubramanian Manoharan <Balasubramanian.Manoharan@caviumnetworks.com>

--- test/performance/odp_l2fwd.c
+++ test/performance/odp_l2fwd.c
@@ -372,7 +372,8 @@ static int run_worker_sched_mode(void *arg)
 
 		/* packets from the same queue are from the same interface */
 		src_idx = odp_packet_input_index(pkt_tbl[0]);
-		assert(src_idx >= 0);
+
+		//assert(src_idx >= 0);
 		dst_idx = gbl_args->dst_port_from_idx[src_idx];
 		fill_eth_addrs(pkt_tbl, pkts, dst_idx);
 
